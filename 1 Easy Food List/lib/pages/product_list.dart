5d418b7f488be83cb2e575268cc64a8a4633dc82
Dwi Randy H <dwirandyherdinanto@gmail.com>
2020-01-23 12:22:09 +0700

    add dismissible widget

--- 1 Easy Food List/lib/pages/product_list.dart
+++ 1 Easy Food List/lib/pages/product_list.dart
@@ -12,29 +12,35 @@ class ProductListPage extends StatelessWidget {
     return ListView.builder(
         itemCount: products.length,
         itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: <Widget>[
-              ListTile(
-                leading: CircleAvatar(
-                    backgroundImage: AssetImage(products[index]['image'])),
-                title: Text(products[index]['title']),
-                subtitle: Text(products[index]['price'].toString()),
-                trailing: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (BuildContext context) {
-                      return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      );
-                    }));
-                  },
+          return Dismissible(
+            key: Key(products[index]['title']),
+            background: Container(
+              color: Colors.red,
+            ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                      backgroundImage: AssetImage(products[index]['image'])),
+                  title: Text(products[index]['title']),
+                  subtitle: Text(products[index]['price'].toString()),
+                  trailing: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return ProductEditPage(
+                          product: products[index],
+                          updateProduct: updateProduct,
+                          productIndex: index,
+                        );
+                      }));
+                    },
+                  ),
                 ),
-              ),
-              Divider()
-            ],
+                Divider()
+              ],
+            ),
           );
         });
   }
